86354347b59f30cd65d960d06355167d6e747815
bjd2385 <brandon.doyle@hqo.co>
2022-05-10 13:19:31 -0400

    Add rev-pack.sh

--- scripts/rev-pack.sh
+++ scripts/rev-pack.sh
@@ -10,16 +10,23 @@ TYP="${2:-commands}"
 
 cd "$SRC/$TYP/" || exit 1
 
-for directory in *; do
-    if [ -d "${directory}" ] && [ -n "$(ls "${directory}")" ]; then
-        for f in "${directory}"/*; do
-            fname="$(basename "$f")"
-            if [ ! -f "${directory}-${fname}" ]; then
-                cp "${f}" "${directory}-${fname}"
+find . -maxdepth 1 -mindepth 1 -type f -print0 | xargs --null -I % basename % | while read -r f; do 
+    subdir="$(echo "$f" | awk -F '-' '{ print $1 }')"
+    if [ -n "$(ls "$subdir")" ]; then
+        fname="${f##"$subdir"-}"
+        if [ -f "${subdir}/${fname}" ]; then
+            if [ "$(md5sum "$f")" = "$(md5sum "${subdir}/${fname}")" ]; then
+                printf "Removing '%s'.\\n" "$f"
+                # shellcheck disable=SC2216
+                yes | rm "$f"
+            else
+                printf "'%s' exists in '%s', but checksums don't match. Skipping removal.\\n" "$fname" "$subdir"
             fi
-        done
-    elif [ -d "${directory}" ]; then
-        printf "INFO: Ignoring '%s', module is empty.\\n" "${SRC}/${TYP}/${directory}"
+        else
+            printf "'%s' does not exist in '%s'.\\n" "$fname" "$subdir"
+        fi
+    else
+        printf "Skipping '%s' as the subdirectory '%s' is empty.\\n" "$f" "${subdir}/\\n"
     fi
 done
 
